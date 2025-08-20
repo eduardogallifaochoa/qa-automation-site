18d409038804d8885bd0191cb23c30c2a38b9152
eduardogallifaochoa <eduardogallifao@gmail.com>
2025-08-19 23:03:03 -0600

    final touches

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import os, sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
 
 @pytest.fixture(scope="function")
 def page():
